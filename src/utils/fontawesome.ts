ca25844536bb27b4738a7a0826102bcdfbd1e0dd
mdrokz <mohammadmunshi@gmail.com>
2023-09-19 21:40:16 +0530

    feat: add common library for fontawesome

---
+++ src/utils/fontawesome.ts
@@ -0,0 +1,24 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faApple, faDiscord, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faArrowRight, faBook, faCircleNodes, faCirclePlus, faCompass, faEnvelope, faMagnifyingGlass, faMobileScreen, faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+
+import {} from '@fortawesome/react-fontawesome';
+
+// import your icons
+
+library.add(
+  faMagnifyingGlass,
+  faXTwitter,
+  faDiscord,
+  faMobileScreen,
+  faCirclePlus,
+  faEnvelope,
+  faArrowRight,
+  faRightToBracket,
+  faApple,
+  faCircleNodes,
+  faCompass,
+  faBook
+  // more icons go here
+);
