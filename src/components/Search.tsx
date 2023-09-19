ce76be3f760ac9e05732e5d93bb86eb5c27b7f89
mdrokz <mohammadmunshi@gmail.com>
2023-09-19 21:40:03 +0530

    chore: improve components UI

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,20 +1,18 @@
-
-
+import { Button } from "./Button"
 
 export const Search = () => {
-    return (<div className="flex flex-col justify-start rounded-md w-[50%] h-max p-2 border border-gray-300">
+    return (<div className="flex flex-col justify-start rounded-md w-[50%] h-full w-full p-2 border hover:border-teal-500 transition duration-300 ease-in-out border-gray-300">
         <div className="flex">
-            <textarea placeholder="Ask anything..." className="w-max h-max border-none outline-none resize-none" />
+            <textarea placeholder="Ask anything..." className="w-full h-full border-none outline-none resize-none" />
         </div>
         <div className="flex justify-between">
             <div className="flex space-x-2 p-2">
-                <button>Focus</button>
-                <button>File</button>
+                <Button label="Focus" icon={["fas","magnifying-glass"]} rounded="-xl"/>
+                <Button label="File" icon={["fas","circle-plus"]} rounded="-xl"/>
             </div>
             <div className="flex space-x-2 p-2">
-                <span>O</span>
                 <button>Copilot</button>
-                <button>Enter</button>
+                <Button icon={["fas","arrow-right"]} rounded="-xl"/>
             </div>
         </div>
     </div>)
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,28 @@
+import { Button } from "../components/Button"
 import { Search } from "../components/Search"
 
 
-const Button = ({ label }: { label: string }) => {
-    return (<button className="text-gray-700 p-1 px-2 rounded transition border-transparent duration-300 ease-in-out hover:border hover:border-gray-100 hover:bg-gray-200">
-        <span>
-            {label}
-        </span>
-    </button>)
-}
-
 export const Home = () => {
     return (
-        <section className="flex justify-center flex-col items-center space-y-4">
-            <header>
-                <span className="font-bold font-mono text-4xl">Where Knowledge Begins</span>
-            </header>
-            <Search />
-            <span>Try asking</span>
-            <div className="flex flex-col space-y-2 items-center">
-                <div className="flex space-x-4">
-                    <Button label="history of Argentina" />
-                    <Button label="unique colorado flowers" />
-                    <Button label="checkout time w hotel cdmx" />
-                    <Button label="how to center a div" />
-                </div>
-                <div className="flex space-x-4">
-                    <Button label="brown dog name ideas" />
-                    <Button label="healthy restaurants in sf" />
-                    <Button label="d/dx x^2 y^4,d/dy x^2 y^4" />
+        <section className="m-auto">
+            <div className="flex justify-center p-20 flex-col items-center space-y-4">
+                <header>
+                    <span className="font-bold font-mono text-4xl">Where Knowledge Begins</span>
+                </header>
+                <Search />
+                <span>Try asking</span>
+                <div className="flex flex-col space-y-2 items-center">
+                    <div className="flex space-x-4">
+                        <Button label="history of Argentina" />
+                        <Button label="unique colorado flowers" />
+                        <Button label="checkout time w hotel cdmx" />
+                        <Button label="how to center a div" />
+                    </div>
+                    <div className="flex space-x-4">
+                        <Button label="brown dog name ideas" />
+                        <Button label="healthy restaurants in sf" />
+                        <Button label="d/dx x^2 y^4,d/dy x^2 y^4" />
+                    </div>
                 </div>
             </div>
         </section>
