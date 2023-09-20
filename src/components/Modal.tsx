1e6b0817bfc85a91c34e1299e004071751e7ae74
mdrokz <mohammadmunshi@gmail.com>
2023-09-20 23:57:17 +0530

    chore: fix modal UI

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,15 +13,15 @@ export const Modal: FC<React.PropsWithChildren<ModalProps>> = ({ closeModal, tit
 
     return (<div className={`relative z-10`} aria-labelledby="modal-title" role="dialog" aria-modal="true">
         {/* Background Overlay */}
-        <div onClick={closeModal} className="fixed inset-0 bg-gray-600 bg-opacity-75 transition-opacity duration-300 ease-in-out"></div>
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-75 transition-opacity duration-300 ease-in-out"></div>
 
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div className="mx-auto w-[50%]">
                 {/* Modal */}
                 <div className="relative transform transition-transform duration-500 ease-out translate-y-full sm:translate-y-1/2 overflow-hidden rounded-lg bg-white text-left shadow-xl">
                     {/* <div className="border-b border-stroke py-4 px-7 dark:border-strokedark"> */}
-                    <div className="w-full flex justify-end p-4">
-                        <div className="flex flex-col justify-center items-center p-2 space-y-2">
+                    <div className="w-full flex p-4">
+                        <div className="flex flex-col flex-1 justify-center items-center p-2 space-y-2">
                             <span className="text-teal-500 text-4xl font-bold">{title}</span>
                             {children}
                         </div>
