87bdb0d84f44fa2a47ab67780d66575771dd2933
mdrokz <mohammadmunshi@gmail.com>
2023-09-21 00:02:24 +0530

    fix: enter bug for search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -8,43 +8,41 @@ export const Search = () => {
     const inputRef = useRef<HTMLTextAreaElement>(null);
     const navigate = useNavigate();
 
-    const [query, setQuery] = useState("");
-
-    const {setShowModal} = useContext(AppContext);
+    const { setShowModal } = useContext(AppContext);
 
     // listen enter event
     useEffect(() => {
 
-        if(inputRef.current == null) return;
+        if (inputRef.current == null) return;
 
         const input = inputRef.current;
 
         input.addEventListener("keydown", (e) => {
-            if (e.key == "Enter" && query != "") {
+            if (e.key == "Enter" && input.value != "") {
                 navigate(`/result?query=${input.value}`)
                 setShowModal(false);
             }
         })
 
-        return () => input?.removeEventListener("keydown", () => { }) 
+        return () => input?.removeEventListener("keydown", () => { })
 
-    },[inputRef])
+    }, [inputRef])
 
     return (<div className="flex flex-col justify-start rounded-md h-full w-full p-2 border hover:border-teal-500 transition duration-300 ease-in-out border-gray-300">
         <div className="flex">
-            <textarea onChange={(e) => setQuery(e.currentTarget.value)} ref={inputRef} placeholder="Ask anything..." className="w-full h-full border-none outline-none resize-none" />
+            <textarea ref={inputRef} placeholder="Ask anything..." className="w-full h-full border-none outline-none resize-none" />
         </div>
         <div className="flex justify-between">
             <div className="flex space-x-2 p-2">
-                <Button label="Focus" icon={["fas","magnifying-glass"]} rounded="-xl"/>
-                <Button label="File" icon={["fas","circle-plus"]} rounded="-xl"/>
+                <Button label="Focus" icon={["fas", "magnifying-glass"]} rounded="-xl" />
+                <Button label="File" icon={["fas", "circle-plus"]} rounded="-xl" />
             </div>
             <div className="flex space-x-2 p-2">
                 <button>Copilot</button>
                 <Button onClick={() => {
-                    if(query != "")
-                    navigate(`/result?query=${query}`)
-                }} icon={["fas","arrow-right"]} rounded="-xl"/>
+                    if (inputRef.current?.value != "")
+                        navigate(`/result?query=${inputRef.current?.value}`)
+                }} icon={["fas", "arrow-right"]} rounded="-xl" />
             </div>
         </div>
     </div>)
